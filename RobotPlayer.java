564f706ee3e5b127b991c67df02933e098608fc8
Nathan Fearnley <nfearnley@gmail.com>
2011-02-20 04:35:03 -0500

    Added comments
    Commented out advanced debug info
    Added error reporting to debug info

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -16,22 +16,29 @@ public class RobotPlayer implements Runnable
 
     public void run()
     {
+        // Loop indefinitely
         while (true)
         {
+            // Prevent errors from crashing program
             try
             {
-                myRC.setIndicatorString(0, myRC.getChassis().toString());
-                String components = "";
-                for (ComponentController comp : myRC.components())
-                {
-                    components += comp.type().toString() + " ";
-                }
-                myRC.setIndicatorString(1, components);
-                myRC.setIndicatorString(2, String.valueOf(Clock.getBytecodeNum()));
-                myRC.yield();                
+                // Debug Information
+                /*
+                 * myRC.setIndicatorString(0, myRC.getChassis().toString());
+                 * String components = ""; for (ComponentController comp :
+                 * myRC.components()) { components += comp.type().toString() +
+                 * " "; } myRC.setIndicatorString(1, components);
+                 * myRC.setIndicatorString(2,
+                 * String.valueOf(Clock.getBytecodeNum()));
+                 */   
+                myRC.addMatchObservation("I am stupid.");
+                myRC.yield();     
             }
             catch (Exception e)
             {
+                // Show any errors in debug info 
+                myRC.setIndicatorString(0, e.getMessage());
+                // Print errors to std err
                 System.err.println("Exception:");
                 e.printStackTrace();
             }
